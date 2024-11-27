49bf1303bda0377fd8b493c31cd5aadad7087e80
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-11-27 11:12:34 -0700

    feature done

--- controllers/postController.js
+++ controllers/postController.js
@@ -17,7 +17,7 @@ const getPosts = async (req, res) => {
         'post.likes',
         'post.created_at'
       );
-    res.json(data);
+    res.status(200).json(data);
   } catch (error) {
     console.error(error);
     res.status(500).json({
@@ -45,11 +45,85 @@ const getPostsByCategory = async (req, res) => {
         'post.created_at',
         'user.name as username'
       );
-    res.json(posts);
+    if (!posts) {
+      return res.status(404).json({ message: `posts not found` });
+    }
+    res.status(200).json(posts);
   } catch (error) {
     console.error('Error fetching posts by category:', error);
     res.status(500).json({ message: 'Error fetching posts', status: 500 });
   }
 };
+const getSinglePost = async (req, res) => {
+  const { postId } = req.params;
+  try {
+    const data = await knex('post')
+      .select(
+        'post.id',
+        'post.title',
+        'post.desc',
+        'post.img',
+        'post.user_id',
+        'user.name as username',
+        'post.category_id',
+        'post.likes',
+        'post.created_at'
+      )
+      .join('user', 'post.user_id', 'user.id')
+      .where('post.id', postId)
+      .first();
 
-export { getPosts, getPostsByCategory };
+    if (!data) {
+      return res
+        .status(404)
+        .json({ message: `Post with ID ${postId} not found` });
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching posts by id:', error);
+    res.status(500).json({ message: 'Error fetching posts', status: 500 });
+  }
+};
+
+const getCommentsByPost = async (req, res) => {
+  const { postId } = req.params;
+  if (!postId) {
+    return res
+      .status(400)
+      .json({ message: 'PostId is missing in the request' });
+  }
+  try {
+    const data = await knex('comment')
+      .select(
+        'comment.id',
+        'comment.comment',
+        'user.name as username',
+        'comment.likes',
+        'comment.created_at',
+        'comment.post_id'
+      )
+      .join('user', 'comment.user_id', 'user.id')
+      .where('comment.post_id', postId);
+
+    if (data.length === 0) {
+      return res
+        .status(200)
+        .json(
+          'No comments yet! Leave a word of encouragement for the author 😊'
+        );
+    }
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching comments by postId:', error);
+    res.status(500).json({ message: 'Error fetching posts', status: 500 });
+  }
+};
+const addPosts = (req, res) => {};
+export {
+  getPosts,
+  getPostsByCategory,
+  getSinglePost,
+  getCommentsByPost,
+  addPosts,
+};
